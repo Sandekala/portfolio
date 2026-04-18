9949eac26ee65ab4a9606a5c4f909e87951dfe11
Sandekala <diimasaziiz@gmail.com>
2026-04-18 22:41:56 +0700

    add sidebar menu

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,9 +1,20 @@
 import type { Metadata } from 'next'
 
+import { LayoutSidebar } from '@/components/layout/sidebar'
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar'
+
 export const metadata: Metadata = {
   title: 'My Dashboard',
 }
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <main className="h-dvh w-screen overflow-auto">{children}</main>
+  return (
+    <SidebarProvider>
+      <LayoutSidebar />
+      <main className="h-dvh w-screen overflow-auto">
+        <SidebarTrigger />
+        {children}
+      </main>
+    </SidebarProvider>
+  )
 }
